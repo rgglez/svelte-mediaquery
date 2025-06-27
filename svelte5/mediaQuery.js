cbd00cf6acafe33e622c876e90e58819c976e1bf
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2025-06-27 00:19:05 -0600

    Fix: runes

--- svelte5/mediaQuery.js
+++ svelte5/mediaQuery.js
@@ -1,18 +1,18 @@
-import { state, effect } from 'svelte';
-
 export function useMediaQuery(query) {
-	const matches = state(false);
-
-	const mediaQuery = window.matchMedia(query);
-
-	effect(() => {
-		matches.set(mediaQuery.matches);
-
-		const handler = (e) => matches.set(e.matches);
-		mediaQuery.addEventListener('change', handler);
-
-		return () => mediaQuery.removeEventListener('change', handler);
-	});
-
-	return matches;
+	const matches = $state(false);
+  
+  	$effect(() => {
+    	const mediaQuery = window.matchMedia(query);
+    	matches = mediaQuery.matches;
+    
+    	const handler = (e) => {
+      		matches = e.matches;
+    	};
+    
+    	mediaQuery.addEventListener('change', handler);
+    
+    	return () => mediaQuery.removeEventListener('change', handler);
+  	});
+  
+  	return () => matches;
 }
